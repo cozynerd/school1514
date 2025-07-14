01f15ea3a8f9244a6bdf5ae1080dadbf97e4c414
cozynerd <cozynerdz@gmail.com>
2025-07-14 13:21:30 +0300

    Add files via upload

---
+++ 05_homework.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+    int t1_h, t1_m, t2_h, t2_m, h, m, ans;
+    
+    cout << "¬ведите врем€ начала урока(hh:mm) - ";
+    scanf("%d:%d", &t1_h, &t1_m);  
+
+    cout << "¬ведите длительность урока в часах - ";
+    cin >> h;
+    
+    cout << "¬ведите длительность урока в минутах - ";
+    cin >> m;
+    
+    ans = (t1_h + h) * 60 + (t1_m + m);
+    t2_h = (ans / 60) % 24;
+    t2_m = ans % 60;
+    
+    printf("%02d:%02d - врем€ конца урока \n", t2_h, t2_m);
+    
+    return 0;
+}
