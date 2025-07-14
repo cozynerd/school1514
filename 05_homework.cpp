318bfe78354154ffec2c697c0ed8683d0d8f13bb
cozynerd <cozynerdz@gmail.com>
2025-07-14 13:33:25 +0300

    Update 05_homework.cpp

--- 05_homework.cpp
+++ 05_homework.cpp
@@ -3,23 +3,22 @@
 using namespace std;
 
 int main() {
-	setlocale(LC_ALL, "Russian");
     int t1_h, t1_m, t2_h, t2_m, h, m, ans;
     
-    cout << "¬ведите врем€ начала урока(hh:mm) - ";
+    cout << "Enter lesson start time (hh:mm) - ";
     scanf("%d:%d", &t1_h, &t1_m);  
 
-    cout << "¬ведите длительность урока в часах - ";
+    cout << "Enter lesson duration hours - ";
     cin >> h;
     
-    cout << "¬ведите длительность урока в минутах - ";
+    cout << "Enter lesson duration minutes - ";
     cin >> m;
     
     ans = (t1_h + h) * 60 + (t1_m + m);
-    t2_h = (ans / 60) % 24;
+    t2_h = ans / 60 % 24;  
     t2_m = ans % 60;
     
-    printf("%02d:%02d - врем€ конца урока \n", t2_h, t2_m);
+    printf("%02d:%02d - lesson end time\n", t2_h, t2_m);
     
     return 0;
 }
